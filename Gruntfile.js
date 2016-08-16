7c0a2f74138b476f1f3ac88dfc897fff664de107
reshmasoap <jahan_z3b@hotmail.com>
2016-08-16 16:59:56 +0500

    added login view and theme integrated

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,7 +23,22 @@ module.exports = function(grunt) {
 			  },
 			  livereload: {
 			  	options: {livereload: true},
-			  	files: ['app/css/*.css']
+			  	files: ['app/css/*.css', 
+			  	'app/src/admin/Add/*.html',
+			  	'app/src/admin/Add/*.js',
+			  	'app/src/admin/Dashboard/*.html',
+			  	'app/src/admin/Dashboard/*.js',
+			  	'app/src/admin/Login/*.html',
+			  	'app/src/admin/Login/*.js',
+			  	'app/src/brand/Add/*.html',
+			  	'app/src/brand/Add/*.js',
+			  	'app/src/brand/Edit/*.html',
+			  	'app/src/brand/Edit/*.js',
+			  	'app/src/voucher/Add/*.html',
+			  	'app/src/voucher/Add/*.js',
+			  	'app/src/voucher/Edit/*.html',
+			  	'app/src/voucher/Edit/*.js'
+			  	]
 			  }
 			} 
 	});
